188f52949d5961915ae50caf97654a8b3c152b2d
Abhi Gupta <abhi.gupta@utexas.edu>
2019-08-12 20:23:31 -0500

    work around timezone issues

--- util.py
+++ util.py
@@ -1,4 +1,11 @@
 from flask import render_template
+import datetime
+from pytz import timezone
+
+dt_fmt = '%Y-%m-%d %H:%M:%S %Z%z'
+
+# assumption made for course purposes that we only use this for now in Texas
+dt_central = timezone('US/Central')
 
 
 def convert(model_obj):
@@ -21,6 +28,8 @@ def convert(model_obj):
             result[key] = new_value
         elif key in ['reservation', 'spot']:
             result[key] = convert(value)
+        elif isinstance(value, datetime.datetime):
+            result[key] = value.astimezone(dt_central).strftime(dt_fmt)
         elif key not in ['_sa_instance_state']:
             result[key] = value
 
